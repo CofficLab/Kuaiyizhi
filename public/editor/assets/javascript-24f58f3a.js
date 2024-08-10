e9fe51b42c516456b600dfcebfc4cc50a464c5d9
GitHub Action of Kuaiyizhi_Editor <action@github.com>
2024-08-10 00:57:20 +0000

    update editor to 0.6.3

--- public/editor/assets/javascript-24f58f3a.js
+++ public/editor/assets/javascript-24f58f3a.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-20748001.js";import"./index-0adb4478.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-39ba72eb.js";import"./index-72db44e0.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
