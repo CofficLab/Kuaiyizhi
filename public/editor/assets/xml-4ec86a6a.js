4e9b8a417ece436dd1f2855c6412b8215749a893
GitHub Action of Kuaiyizhi_Editor <action@github.com>
2024-08-10 11:36:07 +0000

    update editor to 0.6.4

--- public/editor/assets/xml-4ec86a6a.js
+++ public/editor/assets/xml-4ec86a6a.js
@@ -1,4 +1,4 @@
-import{m}from"./index-72db44e0.js";/*!-----------------------------------------------------------------------------
+import{m}from"./index-1b71fac5.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
