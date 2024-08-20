26f0018d314453ad0faed4ea3c2b3a90eff955d3
GitHub Action of Kuaiyizhi_Editor <action@github.com>
2024-08-20 03:51:56 +0000

    update editor to 0.6.20

--- public/editor/assets/freemarker2-1f5b80c0.js
+++ public/editor/assets/freemarker2-1f5b80c0.js
@@ -1,4 +1,4 @@
-import{m as F}from"./index-e11fdc98.js";/*!-----------------------------------------------------------------------------
+import{m as F}from"./index-ed21414e.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
--- public/editor/assets/javascript-579a4349.js
+++ public/editor/assets/javascript-579a4349.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-55cf0cd9.js";import"./index-e11fdc98.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-5161f9ed.js";import"./index-ed21414e.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
