a052a6ae1023faefa580263abd5fda24d20af452
GitHub Action of Kuaiyizhi_Editor <action@github.com>
2024-08-23 15:35:35 +0000

    update editor to 0.7.5

--- public/editor/assets/javascript-fa1db6b3.js
+++ public/editor/assets/javascript-fa1db6b3.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-22a1b110.js";import"./index-7546bf08.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-d783753d.js";import"./index-b6896d71.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
