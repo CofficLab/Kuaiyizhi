7454dcec161b71ba5733faa3bd84e9d7ab9d3d0c
GitHub Action of Kuaiyizhi_Editor <action@github.com>
2024-09-04 09:23:01 +0000

    update editor to 0.9.4

--- public/editor/assets/typescript-5d0625f5.js
+++ public/editor/assets/typescript-5d0625f5.js
@@ -1,4 +1,4 @@
-import{m as a}from"./index-6ac8ef0e.js";/*!-----------------------------------------------------------------------------
+import{m as a}from"./index-ac3a7c1b.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
