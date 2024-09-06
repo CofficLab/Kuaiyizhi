a854fad3f3631ee5a41a38f06d96583cac17703a
GitHub Action of Kuaiyizhi_Editor <action@github.com>
2024-09-06 09:32:57 +0000

    update editor to 0.9.6

--- public/editor/assets/javascript-a0ca875b.js
+++ public/editor/assets/javascript-a0ca875b.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-dbc7a31d.js";import"./index-94ae655e.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-1bb505d4.js";import"./index-0b46355c.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
--- public/editor/assets/razor-59bf38b8.js
+++ public/editor/assets/razor-59bf38b8.js
@@ -1,4 +1,4 @@
-import{m as s}from"./index-94ae655e.js";/*!-----------------------------------------------------------------------------
+import{m as s}from"./index-0b46355c.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
