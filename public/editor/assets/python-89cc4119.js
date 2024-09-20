3672658c1bf5faf8c10fa6769b1cb8fa70135723
GitHub Action of Kuaiyizhi_Editor <action@github.com>
2024-09-20 10:36:57 +0000

    update editor to 0.9.20

--- public/editor/assets/python-89cc4119.js
+++ public/editor/assets/python-89cc4119.js
@@ -1,4 +1,4 @@
-import{m as a}from"./index-a1835af7.js";/*!-----------------------------------------------------------------------------
+import{m as a}from"./index-9c0ea397.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
