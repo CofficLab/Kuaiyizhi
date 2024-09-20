d74525291f5142b7787073ddf8c60600639fdada
GitHub Action of Kuaiyizhi_Editor <action@github.com>
2024-09-20 05:36:29 +0000

    update editor to 0.9.18

--- public/editor/assets/javascript-b12e9328.js
+++ public/editor/assets/javascript-b12e9328.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-e9296cfe.js";import"./index-a1954671.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-070bbc20.js";import"./index-e6baa1e2.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
