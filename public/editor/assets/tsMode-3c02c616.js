cfbb4a9e194f861ab3dab208771e9f50ebced1b2
GitHub Action of Kuaiyizhi_Editor <action@github.com>
2024-09-21 13:57:14 +0000

    update editor to 0.9.24

--- public/editor/assets/tsMode-3c02c616.js
+++ public/editor/assets/tsMode-3c02c616.js
@@ -1,4 +1,4 @@
-import{t as I,m as N}from"./index-8ffbe5a1.js";/*!-----------------------------------------------------------------------------
+import{t as I,m as N}from"./index-515a8412.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
