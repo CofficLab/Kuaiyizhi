0e3be89010fa0d05ff7b1e5bbd5ac5e244391048
GitHub Action of Kuaiyizhi_Editor <action@github.com>
2024-10-11 03:54:39 +0000

    update editor to 0.9.26

--- public/editor/assets/javascript-64210af7.js
+++ public/editor/assets/javascript-64210af7.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-8903c774.js";import"./index-3f431d84.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-2b2de5ba.js";import"./index-bc4493ce.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
--- public/editor/assets/tsMode-cfcbe68e.js
+++ public/editor/assets/tsMode-cfcbe68e.js
@@ -1,4 +1,4 @@
-import{t as I,m as N}from"./index-3f431d84.js";/*!-----------------------------------------------------------------------------
+import{t as I,m as N}from"./index-bc4493ce.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
