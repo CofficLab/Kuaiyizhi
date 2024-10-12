c702efadd82fa9b6d6ee820dfffd19d91260fdbe
GitHub Action of Kuaiyizhi_Editor <action@github.com>
2024-10-12 08:27:05 +0000

    update editor to 0.9.28

--- public/editor/assets/javascript-fbf3cc8a.js
+++ public/editor/assets/javascript-fbf3cc8a.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-30dd11ab.js";import"./index-57659cb5.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-e869d83e.js";import"./index-97ef57ef.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.50.0(c321d0fbecb50ab8a5365fa1965476b0ae63fc87)
  * Released under the MIT license
