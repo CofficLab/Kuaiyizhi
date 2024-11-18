28c6420280429b35ecf7f4ec4f7ca9ff4fca41ec
Nookery <nooks@qq.com>
2024-11-18 22:29:00 +0800

    🎨 Chore:  Minor adjustments

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@ import { Editor, EditorFactory } from '@coffic/juice-editor';
 
 EditorFactory.register('juice-editor', {
   onCreate: (editor) => {
-    editor.enableVerboseMode();
+    // editor.enableVerboseMode();
     editor.enableSlotListener();
     editor.disableLocalStorage();
     editor.setReadOnly();
