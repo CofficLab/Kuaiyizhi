6bf264ff5904249d6df89c35c53661f219297b9b
Nookery <nooks@qq.com>
2025-03-08 19:02:04 +0800

    🐛 fix: resolve memory leak issue

--- src/pages/api/oauth.ts
+++ src/pages/api/oauth.ts
@@ -8,11 +8,17 @@ export const POST: APIRoute = async ({ redirect, url, locals }) => {
   const endpoint = process.env.PUBLIC_APPWRITE_ENDPOINT;
   const project = process.env.PUBLIC_APPWRITE_PROJECT;
   const key = process.env.APPWRITE_KEY
+  const endpoint2 = import.meta.env.PUBLIC_APPWRITE_ENDPOINT;
+  const project2 = import.meta.env.PUBLIC_APPWRITE_PROJECT;
+  const key2 = import.meta.env.APPWRITE_KEY;
 
   console.log('endpoint -------------', endpoint);
   console.log('project -------------', project);
   console.log('key -------------', key);
   console.log('locals -------------', locals);
+  console.log('endpoint2 -------------', endpoint2);
+  console.log('project2 -------------', project2);
+  console.log('key2 -------------', key2);
 
   if (!endpoint || !project || !key) {
     throw new Error("Missing Appwrite environment variables");
