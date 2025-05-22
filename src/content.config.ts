2d7d6c5634f5df975cd0e0d16565e325f8b6f8a8
nookery <nooks@foxmail.com>
2025-05-22 12:01:08 +0800

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,65 +1,9 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { makeBlogCollection, makeMetaCollection, makeLessonCollection, makeExperimentCollection, makeCourseCollection } from '@coffic/cosy-ui/collection';
 
 export const collections = {
-	blogs: defineCollection({
-		loader: glob({
-			pattern: '**/*.{md,mdx}',
-			base: './content/blogs',
-		}),
-		schema: z.object({
-			title: z.string().optional(),
-			description: z.string().optional(),
-			tags: z.array(z.string()).optional(),
-			date: z.date().optional(),
-			authors: z.array(z.object({
-				name: z.string(),
-				picture: z.string().optional(),
-				url: z.string().optional(),
-			})).optional(),
-		}),
-	}),
-
-	meta: defineCollection({
-		loader: glob({
-			pattern: '**/*.{md,mdx}',
-			base: './content/meta',
-		}),
-
-	}),
-
-	courses: defineCollection({
-		loader: glob({
-			pattern: '**/*.{md,mdx}',
-			base: './content/courses',
-		}),
-
-	}),
-
-	lessons: defineCollection({
-		loader: glob({
-			pattern: '**/{zh-cn,en}/**/*.{md,mdx}',
-			base: './content/lessons',
-		}),
-		schema: z.object({
-			title: z.string(),
-			description: z.string().optional(),
-			authors: z.array(z.object({
-				name: z.string(),
-				picture: z.string().optional(),
-			})).optional(),
-		}),
-	}),
-
-	experiments: defineCollection({
-		loader: glob({
-			pattern: '**/*.{md,mdx}',
-			base: './content/experiments',
-		}),
-		schema: z.object({
-			title: z.string(),
-			description: z.string().optional(),
-			pubDate: z.date().optional(),
-		}),
-	}),
+	blogs: makeBlogCollection('./content/blogs'),
+	meta: makeMetaCollection('./content/meta'),
+	courses: makeCourseCollection('./content/courses'),
+	lessons: makeLessonCollection('./content/lessons'),
+	experiments: makeExperimentCollection('./content/experiments'),
 };
