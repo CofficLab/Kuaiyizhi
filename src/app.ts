6c01b35e6edb465d149e4603ac24051ee239faca
Nookery <nooks@qq.com>
2025-05-22 03:04:01 +0000

    ✨ feat: enhance UI components with new styles and search functionality

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { LangPackage } from '@coffic/cosy-ui';
-
+import LinkDB from '@/utils/link';
 class App {
     companyName = LangPackage.setZh('青岛岳亿网络科技有限公司')
         .setEn('Qingdao Yueyi Network Technology Co., Ltd.')
@@ -8,6 +8,24 @@ class App {
     slogan = LangPackage.setZh('精彩软件，有爱创造').setEn('Wonderful Software, Built with Love')
     inspirationalSlogan = LangPackage.setZh('也许，这是一番成就的开始').setEn('Perhaps, this is the beginning of achievement')
     copyright = LangPackage.setZh('版权所有').setEn('All rights reserved')
+    keywords = 'Cisum,GitOk,TravelMode,Netto,CosyUI,菲克与象彩彩'
+    siteName = '快易知'
+    getNavItems(lang: string) {
+        return [
+            {
+                href: LinkDB.getHomeLink(lang),
+                label: lang === 'zh-cn' ? '首页' : 'Home',
+            },
+            {
+                href: LinkDB.getLessonsLink(lang),
+                label: 'Build Your Own X',
+            },
+            {
+                href: LinkDB.getBlogsLink(lang),
+                label: lang === 'zh-cn' ? '博客' : 'Blogs',
+            },
+        ]
+    }
 
     socialLinks = ['https://github.com/CofficLab']
     products =
